7adde61f794a743db229b4fb3f21e8e0c34cfd93
Kushagra <kushagram2000@gmail.com>
2022-10-03 19:00:54 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is a Todo website in which you can add your daily tasks. 
+            <p>I have made this application using HTML, CSS, JavaScript, Bootstrap and React. This application not only saves yor time but also keeps your notes or your daily task safe and secure. Thank you for using our Todo website </p>
+        </div>
+    )
+}
